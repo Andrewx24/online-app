3ddea4901c420d4e6260dc986d722ca29bfd7674
Andrew Aliaj <andyaliaj@gmail.com>
2024-07-09 11:28:07 -0400

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "@/styles/globals.css"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Boring Coffee Shop",
+  title: "Tech Brew Coffee Shop",
   description: " Online coffee shop for the boring people.", 
 };
 
